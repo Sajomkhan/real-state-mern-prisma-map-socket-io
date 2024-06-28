599d24b4ba888fe59be543820362968916077ca0
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-06-29 02:38:04 +0600

    Navebar updated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav>
       <div className="left">
@@ -8,14 +11,34 @@ const Navbar = () => {
           <img src="/logo.png" alt="" />
           <span>RealEstate</span>
         </a>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-        <a href="/agents">Agents</a>
+        <div className="navlink">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+          <a href="/agents">Agents</a>
+        </div>
       </div>
       <div className="right">
-      <a href="/">Signin</a>
-      <a href="/">Sign up</a>
+        <a href="/">Sign in</a>
+        <a href="/" className="signup">
+          Sign up
+        </a>
+        {/* =======For Mobile Menu ========= */}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen(!open)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+          <a href="/agents">Agents</a>
+          <a href="/signin">Sign in</a>
+          <a href="/signup">Sign up</a>
+        </div>
       </div>
     </nav>
   );
